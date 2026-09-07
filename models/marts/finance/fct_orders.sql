62b5227e49f0e45b3bfba5544f9159d371b1f180
ali.aboualam@artefact.com <ali.aboualam@artefact.com>
2026-09-07 13:44:29 +0000

    referenced sources in staging models

--- models/marts/finance/fct_orders.sql
+++ models/marts/finance/fct_orders.sql
@@ -1,4 +1,4 @@
-select orders.order_id, orders.customer_id, payments.amount
+select orders.order_id, orders.customer_id, payments.payment_amount
 from {{ ref('stg_jaffle_shop__orders') }} as orders
 join {{ ref('stg_stripe__payments') }} as payments
-on payments.orderid = orders.order_id
+on payments.order_id = orders.order_id
--- models/marts/marketing/dim_customers.sql
+++ models/marts/marketing/dim_customers.sql
@@ -40,6 +40,7 @@ final as (
         customers.last_name,
         customer_orders.first_order_date,
         customer_orders.most_recent_order_date,
+        customer_orders.lifetime_value,
         coalesce(customer_orders.number_of_orders, 0) as number_of_orders
 
     from customers
