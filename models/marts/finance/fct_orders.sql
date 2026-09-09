4d3d706b61542b75a27e73b22321c2c6aa2f3d98
Ali Aboualam <ali.aboualam@artefact.com>
2026-09-09 16:07:05 +0000

    changes...

--- models/marts/finance/fct_orders.sql
+++ models/marts/finance/fct_orders.sql
@@ -1,3 +1,9 @@
+{{
+    config(
+        materialized='ephemeral'
+    )
+}}
+
 select orders.order_id, orders.customer_id, payments.amount_in_cents
 from {{ ref('stg_jaffle_shop__orders') }} as orders
 join {{ ref('stg_stripe__payments') }} as payments
